bad374034fc80abe23c276adef957f95e75650dc
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-13 19:58:45 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+# def update_array_at_with(array, index, value = "X")
